c600ba786657a84842d2f5d33c3c9ab3f02efa8f
Zenbook <komar86311@gmail.com>
2019-06-11 20:17:25 +0200

    Added more necessary function declarations

--- include/NegationNormalForm.h
+++ include/NegationNormalForm.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include <string>
+/* Returns a formula with replaced implications */
+std::string replaceImplication(const std::string& formula, const unsigned int& implicationPos);
+
+/* Returns a formula with replaced equalities */
+std::string replaceEquality(const std::string& formula, const unsigned int& equalityPos);
+
 /* Returns a formula with all reduntant operators (implication, equality) brought to a normal form */
 std::string replaceRedundantOperators(const std::string formula);
 
