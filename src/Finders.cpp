3cfd9b340a5530743f540c0e34a2d613b1fc9b01
Zenbook <komar86311@gmail.com>
2019-06-11 18:53:22 +0200

    Added two definitions

---
+++ src/Finders.cpp
@@ -0,0 +1,51 @@
+#include "Finders.h"
+#include "operators.h"
+
+int findNearestNegation(const std::string& formula, const unsigned int& startPos){
+    for(unsigned int pos=startPos; pos<formula.length(); pos++){
+        if(formula[pos]==NOT)
+            return pos;
+    }
+    return -1; //If nothing has been found
+}
+
+int findBracketCompletion(const std::string& formula, const unsigned int& pos){
+    unsigned int count=1; // Counts of the 
+    char bracket=formula[pos];
+    if(bracket==P_CLOSE){ // If parenthesis on a given pos is a closed parenthesis, look for matching open parenthesis.
+        for(unsigned int ptr=pos-1; ptr>=0; ptr--){
+            if(formula[ptr]==P_OPEN && count==1) 
+                return ptr;
+            else if(formula[ptr]==P_CLOSE)
+                count++;
+            else if(formula[ptr]==P_OPEN)
+                count--;
+        }
+    } else { // If parenthesis on a given pos is an open parenthesis, look for matching closed parenthesis.
+        for(unsigned int ptr=pos+1; ptr!=0; ptr++){
+            if(formula[ptr]==P_CLOSE && count==1) 
+                return ptr;
+            else if(formula[ptr]==P_OPEN)
+                count++;
+            else if(formula[ptr]==P_CLOSE)
+                count--;
+        }
+    }
+    return -1; //If nothing has been found
+}
+
+bool isBeforeBracketOrQuantifier(const std::string& formula, const unsigned int& pos){
+
+}
+
+bool isPredicate(const std::string& formula, const unsigned int& pos){
+
+}
+
+unsigned int findPredecessor(const std::string& formula, const unsigned int& pos){
+
+}
+
+unsigned int findSuccessor(const std::string& formula, const unsigned int& pos){
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include "NegationNormalForm.h"
-
+#include "Finders.h"
 #include <iostream>
 
 int main(){
-    std::string line;
-    std::getline(std::cin, line);
-    std::cout << line << std::endl;
+    std::string line="(a(q,e))";
+    int i = findBracketCompletion(line, 7);
+    std::cout << i << std::endl;
     std::cin.get();
     return 0;
 }
