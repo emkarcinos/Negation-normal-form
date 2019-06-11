91342597c30ff9860872bcc887d105f1bd2b74df
Marcin Kostrzewski <komar86311@gmail.com>
2019-06-11 17:48:15 +0200

    Added few declarations

--- include/NegationNormalForm.h
+++ include/NegationNormalForm.h
@@ -11,3 +11,13 @@
 #define P_CLOSE ')'
 
 #include <string>
+
+/* Returns an index of the first negation occurrence from a formula, with a given start position. Returns -1 if it wasn't found.*/
+int findNearestNegation(const std::string& formula, const unsigned int& startPos);
+
+/* Cheks whether the next character in a formula is a bracket */
+bool isBeforeBracket(const std::string& formula, const unsigned int& pos);
+
+/* Checks if there's a double negation */
+bool isDoubleNegation(const std::string& formaula, const unsigned int& pos);
+
