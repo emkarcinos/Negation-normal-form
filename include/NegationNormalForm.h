379c06316f5a7b26d24d6270e83bf226c1c4ef6e
Marcin Kostrzewski <komar86311@gmail.com>
2019-06-11 17:36:15 +0200

    Added NegationNormalForm header and definition

---
+++ include/NegationNormalForm.h
@@ -0,0 +1,13 @@
+/* Logical operators */
+#define NOT     '!'
+#define AND     '^'
+#define OR      'v'
+#define EQUAL   '='
+#define IMPLIC  '>'
+#define Q_EXIST 'E'
+#define Q_UNI   'V'
+/* Parenthesis */
+#define P_OPEN  '('
+#define P_CLOSE ')'
+
+#include <string>
---
+++ src/NegationNormalForm.cpp
@@ -0,0 +1,1 @@
+#include "NegationNormalForm.h"
