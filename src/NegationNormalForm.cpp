92bc6c2ed77802fb6d75c7beb46b566178614890
Zenbook <komar86311@gmail.com>
2019-06-12 08:56:08 +0200

    replaceEquality function has been written

--- src/NegationNormalForm.cpp
+++ src/NegationNormalForm.cpp
@@ -2,6 +2,7 @@
 #include "Finders.h"
 #include "Tools.h"
 #include "operators.h"
+#include <sstream>
 
 std::string applyDeMorgan(const std::string& formula, const unsigned int& pos){
 
@@ -16,15 +17,29 @@ std::string replaceImplication(const std::string& formula, const unsigned int& i
 }
 
 std::string replaceEquality(const std::string& formula, const unsigned int& equalityPos){
-
+    std::string result = formula;
+    unsigned int predecessorPos = findPredecessor(formula, equalityPos);
+    unsigned int successorEnd = findSuccessorEnd(formula, equalityPos);
+    std::string leftSide=formula.substr(predecessorPos,equalityPos-predecessorPos);
+    std::string rightSide=formula.substr(equalityPos+1, successorEnd-equalityPos);
+    std::stringstream lhsStream;
+    std::stringstream rhsStream;
+    lhsStream << "(!" << leftSide << OR << rightSide << ")^";
+    rhsStream << "(!" << rightSide << OR << leftSide << ")";
+    std::string finalReplacement=lhsStream.str() + rhsStream.str();
+    result=replaceSlice(result, predecessorPos, successorEnd+1, finalReplacement);
+    return result;
 }
 
 std::string replaceRedundantOperators(const std::string formula){
-    unsigned int currentPos=0;
-    std::string result="";
-    for(unsigned int curs=0; curs<formula.length(); curs++){
-        curs=findReduntantOperator(formula, curs);
-
+    std::string result=formula;
+    int op=findReduntantOperator(result);
+    while(op!=-1){
+        if(result[op]==IMPLIC)
+            result=replaceImplication(result, op);
+        else if (result[op]==EQUAL)
+            result=replaceEquality(result, op);
+        op=findReduntantOperator(result);
     }
     return result;
 }
